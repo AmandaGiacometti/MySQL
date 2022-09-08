ca59e19a34afc026ee6c820671609d67fab92f28
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2022-09-08 00:16:02 -0300

    Concluído!

--- Tarefa02/Exercicio01Game.sql
+++ Tarefa02/Exercicio01Game.sql
@@ -1,40 +1,37 @@
-create database db_pizzaria_legal; 
-use db_pizzaria_legal; 
-create table tb_categorias(  
-id bigint(5) auto_increment,  
-tipo varchar(30) not null, 
-preco bigint (15) not null, 
+create database db_generation_game_online; 
+use db_generation_game_online; 
+create table tb_classes(  
+Id bigint(5) auto_increment,  
+mundo varchar(30) not null, 
+fase varchar(30) not null, 
 primary key(id)  
 ) ;  
-insert into tb_categorias(tipo,preco) values("Tradicional",40);  
-insert into tb_categorias(tipo,preco) values("Tradicional",45);  
-insert into tb_categorias(tipo,preco) values("Tradicional",50);  
-insert into tb_categorias(tipo,preco) values("Especial",55);  
-insert into tb_categorias(tipo,preco) values("Especial",60);  
-use db_pizzaria_legal; 
-
-create table tb_pizzas(  
+insert into tb_classes(mundo,fase) values("Mundo Urbano","Escola Abandonada");  
+insert into tb_classes(mundo,fase) values("Mundo Urbano","Hospital Macabro");  
+insert into tb_classes(mundo,fase) values("Mundo Florestal","Pântano Proibido");  
+insert into tb_classes(mundo,fase) values("Mundo Florestal","Jardim Venenoso");  
+insert into tb_classes(mundo,fase) values("Mundo Aquático","Lagoa Sem Fundo");  
+use db_generation_game_online; 
+create table tb_personagens(  
 Id bigint auto_increment, 
-sabor varchar(30) not null,
-queijo varchar(20), 
-bordarecheada boolean,
-vegetariana boolean,
-categorias_id bigint not null, 
+nome varchar(30) not null, 
+tipo varchar(30) not null, 
+ataque bigint (10) not null, 
+defesa bigint(10) not null, 
+classes_id bigint not null, 
 primary key (id),   
-foreign key(categorias_id) references tb_categorias (id)   
+foreign key(classes_id) references tb_classes (id)   
 );  
-
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Mussarela","Mussarela",false,true,1);
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Frango","Catupiry",false,false,1); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Margherita","Parmesão",false,true,2); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Palmito","Catupiry",true,true,2); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Calabresa","Sem queijo",true,false,3); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Abobrinha","Parmesão",true,true,4); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Portuguesa","Mussarela",true,false,4); 
-insert into tb_pizzas(sabor,queijo,bordarecheada,vegetariana,categorias_id) values("Caprese","Musssarela de Búfala",false,true,5); 
-select*from tb_categorias where preco<45;
-select*from tb_categorias where preco between 50 and 100; 
-select*from tb_pizzas where sabor like "%m%"; 
-select*from tb_pizzas inner join tb_categorias on tb_categorias.id=tb_pizzas.categorias_id; 
-select*from tb_pizzas inner join tb_categorias where tipo = "Tradicional";
-
+insert into tb_personagens(nome,tipo,ataque,defesa,classes_id) values("Acquamarine","Sereia",3500,1500,5); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("Rumpeltiskin","Gnomo",2500,6000,3); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("Lupin","Lobisomem",5000,3500,1); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("José","Humano",1500,1500,2); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("Legolas","Elfo",5500,4500,3); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("Penadinho","Espírito",1000,6000,2); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("Drácula","Vampiro",4500,1500,1); 
+insert into tb_personagens(nome,tipo,ataque,defesa, classes_id) values("LanHua","Fada",1500,4000,4); 
+select*from tb_personagens where ataque>2000; 
+select*from tb_personagens where ataque between 1000 and 2000; 
+select*from tb_personagens where nome like "%c%"; 
+select*from tb_personagens inner join tb_classes on tb_classes.id=tb_personagens.classes_id; 
+select nome from tb_personagens inner join tb_classes where classes_id = 2; 
